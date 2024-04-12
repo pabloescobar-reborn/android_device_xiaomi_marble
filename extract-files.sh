6c3df1b807de3dbad95cef555950f050e976207b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-04-12 06:46:12 +0530

    marble: Inherit from xiaomi-sm8450-common
    Change-Id: I861b3c6fc6862ee1395c917784c0086d9016ea12

--- extract-files.sh
+++ extract-files.sh
@@ -6,86 +6,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
-DEVICE=marble
-VENDOR=xiaomi
+export DEVICE=marble
+export DEVICE_COMMON=sm8450-common
+export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Default to sanitizing the vendor folder before extraction
-CLEAN_VENDOR=true
-
-KANG=
-SECTION=
-
-while [ "${#}" -gt 0 ]; do
-    case "${1}" in
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
-    esac
-    shift
-done
-
-if [ -z "${SRC}" ]; then
-    SRC="adb"
-fi
-
-function blob_fixup() {
-    case "${1}" in
-        vendor/bin/hw/android.hardware.security.keymint-service-qti | vendor/lib64/libqtikeymint.so)
-            "${PATCHELF}" --add-needed "android.hardware.security.rkp-V1-ndk_platform.so" "${2}"
-            ;;
-        vendor/bin/hw/dolbycodec2 | vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service | vendor/bin/hw/vendor.dolby.media.c2@1.0-service)
-            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
-            ;;
-        vendor/bin/hw/mfp-daemon | vendor/lib64/hw/displayfeature.default.so | vendor/lib64/hw/audio.primary.taro.so)
-            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
-            ;;
-        vendor/bin/hw/vendor.qti.hardware.display.composer-service)
-            "${PATCHELF}" --remove-needed "libutils.so" "${2}"
-            "${PATCHELF}" --add-needed "libutils-v32.so" "${2}"
-            "${PATCHELF}" --add-needed "libutils-shim.so" "${2}"
-            ;;
-        vendor/bin/hw/vendor.qti.secure_element@1.2-service)
-            "${PATCHELF}" --replace-needed "jcos_nq_client-v1.so" "jcos_nq_client.so" "${2}"
-            "${PATCHELF}" --replace-needed "ls_nq_client-v1.so" "ls_nq_client.so" "${2}"
-            "${PATCHELF}" --replace-needed "se_nq_extn_client-v1.so" "se_nq_extn_client.so" "${2}"
-            ;;
-        vendor/etc/camera/marble*_motiontuning.xml)
-            sed -i 's/xml=version/xml\ version/g' "${2}"
-            ;;
-        vendor/etc/camera/pureView_parameter.xml)
-            sed -i "s/=\([0-9]\+\)>/=\"\1\">/g" "${2}"
-            ;;
-    esac
-}
-
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
-
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-
-"${MY_DIR}/setup-makefiles.sh"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
--- rootdir/bin/init.marble.perf.sh
+++
@@ -1,7 +0,0 @@
-#!/vendor/bin/sh
-#
-# Copyright (C) 2024 Paranoid Android
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Placeholder for future
